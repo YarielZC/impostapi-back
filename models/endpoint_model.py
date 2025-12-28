5ddc5db0ec6eb330f541fb1c7e145ed9f89fb94f
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 14:06:55 -0500

    Create endpoint model

---
+++ models/endpoint_model.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+class EndpointCreate(BaseModel):
+  name: str
+  description: str | None = None
+  method: str
+  path_url: str
+  response: str | None = None
+  status_code: int
+  delay: int | None = None
+  is_public: bool
+
+class EndpointResponse(EndpointCreate):
+  id: str = Field(alias='_id')
+  project_id: str
+  update_at: datetime = Field(default_factory=lambda: datetime.now())
+  created_at: datetime = Field(default_factory=lambda: datetime.now())
+
+  model_config = ConfigDict(
+    populate_by_name=True,
+  )
