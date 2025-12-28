e4b8d5a2038c21d59352c61b1ff0805b6ffb1258
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 18:45:01 -0500

    add secrets keys to read from enviroment

--- core/settings.py
+++ core/settings.py
@@ -5,6 +5,9 @@ from pydantic import Field
 class Settings(BaseSettings):
     MONGO_URI: str = Field(alias="MONGO_URI")
     DB_NAME: str = Field(alias="DB_NAME")
+    TOKEN_DURATION: int = Field(alias="TOKEN_DURATION")
+    SECRET_TOKEN_KEY: str = Field(alias="SECRET_TOKEN_KEY")
+    ALGORITHM_CRYPT: str = Field(alias="ALGORITHM_CRYPT")
     model_config = SettingsConfigDict(env_file=".env")
 
 
