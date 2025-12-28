abbd9837d1928b9e553d5b6536db8831307cb242
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 15:48:18 -0500

    Refact User Repository to convert automatically the result to user response

--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -1,8 +1,10 @@
 from pymongo.asynchronous.database import AsyncDatabase
 
-from models.user_model import UserCreate
+from db.connection import get_database
+from models.user_model import UserCreate, UserResponse
 from .base_repository import BaseRepository
 
+from fastapi import Depends
 
 class UserRepository(BaseRepository):
   def __init__(self, database: AsyncDatabase) -> None:
@@ -13,7 +15,15 @@ class UserRepository(BaseRepository):
     return await self.db.find_one({field: value})
   
   async def find_one_by_id(self, id: str):
-    return await self.db.find_one({'_id': self._to_object_id(id)})
+    result = await self.db.find_one({'_id': self._to_object_id(id)})
+
+    try:
+      result = self._map_doc(result)
+    except:
+      return None
+
+    return UserResponse(**result)
+
   
   async def insert_one(self, user: UserCreate):
     result = await self.db.insert_one(dict(user))
@@ -22,3 +32,6 @@ class UserRepository(BaseRepository):
   async def delete_user_by_id(self, id: str):
     result = await self.db.delete_one({'_id': id})
     return result.deleted_count
+
+def get_user_repository(database: AsyncDatabase = Depends(get_database)):
+  return UserRepository(database=database)
