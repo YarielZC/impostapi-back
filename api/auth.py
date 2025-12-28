5b36bc50f3210372def19810ae3130a62fc02518
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 18:43:59 -0500

    Create login function (endpoint)

--- api/auth.py
+++ api/auth.py
@@ -1,7 +1,12 @@
+from datetime import datetime, timedelta
 from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+import jwt
 from passlib.context import CryptContext 
 
-from models.user_model import UserResponse, UserCreate
+from core.settings import settings
+
+from models.user_model import UserDB, UserResponse, UserCreate
 
 from repositories.user_repository import UserRepository, get_user_repository
 
@@ -30,5 +35,33 @@ async def register_user(user: UserCreate, repo: UserRepository = Depends(get_use
     raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                         detail='User not created but it should')
   
-  return new_user
+  return UserResponse(**new_user)
 
+@auth_router.post('/login', response_model=dict, status_code=status.HTTP_202_ACCEPTED)
+async def login(repo: UserRepository = Depends(get_user_repository), form: OAuth2PasswordRequestForm = Depends()):
+  user = await repo.find_one_by_field('email', form.username)
+  if not user:
+    user = await repo.find_one_by_field('username', form.username)
+
+  if not user:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail='Username or password is wrong')
+  
+  user = UserDB(**user)
+
+  if not crypt.verify(form.password, user.password):
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail='Username or password is wrong')
+  
+  access_token_expiration = datetime.now() + timedelta(minutes=settings.TOKEN_DURATION)
+  access_token = {
+    'sub': user.username,
+    'exp': access_token_expiration
+  }
+
+  return {
+    'access_token': jwt.encode(access_token,
+                               key=settings.SECRET_TOKEN_KEY,
+                               algorithm=settings.ALGORITHM_CRYPT),
+    'token_type': 'bearer'
+  }
