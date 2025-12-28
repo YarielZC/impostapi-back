c09063ef0906b87697055127ad7018b00d06fcb0
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 15:49:00 -0500

    Add register user endpoint

--- api/auth.py
+++ api/auth.py
@@ -1,5 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status, Depends
+from passlib.context import CryptContext 
+
+from models.user_model import UserResponse, UserCreate
+
+from repositories.user_repository import UserRepository, get_user_repository
+
+from validations.user_validation import user_validation
+
+crypt = CryptContext(schemes=['bcrypt'])
 
 auth_router = APIRouter(prefix='/auth',
                    tags=['Authentication'],
                    )
+
+@auth_router.post('/register_user', response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+async def register_user(user: UserCreate, repo: UserRepository = Depends(get_user_repository)):
+
+  await user_validation(user, repo)
+
+  user_dict = user.model_dump()
+
+  user_dict['password'] = crypt.hash(user_dict['password'])
+
+  id = await repo.insert_one(UserCreate(**user_dict))
+
+  new_user = await repo.find_one_by_id(id)
+
+  if not new_user:
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail='User not created but it should')
+  
+  return new_user
+
