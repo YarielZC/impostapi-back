5c4a4571a6f526bda02a96e99800233d47cb094d
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 21:38:38 -0500

    Refactorize endpoint model to use correctly twice model (create and response)

--- models/endpoint_model.py
+++ models/endpoint_model.py
@@ -6,16 +6,16 @@ class EndpointCreate(BaseModel):
   description: str | None = None
   method: str
   path_url: str
-  response: str | None = None
+  response: str | dict | None = None
   status_code: int
   delay: int | None = None
   is_public: bool
-
-class EndpointResponse(EndpointCreate):
-  id: str = Field(alias='_id')
   project_id: str
   update_at: datetime = Field(default_factory=lambda: datetime.now())
   created_at: datetime = Field(default_factory=lambda: datetime.now())
+
+class EndpointResponse(EndpointCreate):
+  id: str = Field(alias='_id')
 
   model_config = ConfigDict(
     populate_by_name=True,
