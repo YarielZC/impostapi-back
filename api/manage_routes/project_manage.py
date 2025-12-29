2572d1ad2ae6fefb1f49c658a57701d3e1caee50
YarielZC <yarielzamoracueto@gmail.com>
2025-12-29 00:37:51 -0500

    Add delete endpoint to delete projects

--- api/manage_routes/project_manage.py
+++ api/manage_routes/project_manage.py
@@ -3,6 +3,8 @@ from logic.jwt_auth_user import auth_user
 from models.project_model import ProjectCreate, ProjectResponse
 from models.user_model import UserResponse
 from repositories.project_repository import ProjectRepository, get_project_repository
+from repositories.endpoint_repository import EndpointRepository, get_endpoint_repository
+from logic.owner_project import owner_project_validate
 
 manage_project_router = APIRouter(prefix='/project',
                    tags=['Manage Project'],
@@ -27,3 +29,29 @@ async def create_project(project: ProjectCreate, repo: ProjectRepository = Depen
                   detail='Unknow error')
 
   return ProjectResponse(**result)
+
+@manage_project_router.delete('/delete/{id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_project(id: str, repo: ProjectRepository = Depends(get_project_repository), repoEndpoint: EndpointRepository = Depends(get_endpoint_repository), user: UserResponse = Depends(auth_user)):
+
+  await owner_project_validate(project_id=id,
+                               repo=repo,
+                               user=user)
+  
+  result = await repoEndpoint.delete_many({'project_id': id})
+  result = await repo.delete_by_id(id)
+
+  if result != 1:
+    raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                        detail='Unknow error')
+
+@manage_project_router.get('/count_endpoints/{id}', response_model=int, status_code=status.HTTP_200_OK)
+async def count_endpoints(id: str, repo: ProjectRepository = Depends(get_project_repository), repoEndpoint: EndpointRepository = Depends(get_endpoint_repository), user: UserResponse = Depends(auth_user)):
+  
+  await owner_project_validate(project_id=id,
+                               repo=repo,
+                               user=user)
+  
+  result = await repoEndpoint.count_endpoints(id)
+  return len(await result.to_list())
+   
+
