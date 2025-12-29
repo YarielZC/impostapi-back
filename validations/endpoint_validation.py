e26331092cf2c081e13c16139578ffe06222fb15
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 23:16:03 -0500

    Validate that a endpoint be from a project

--- validations/endpoint_validation.py
+++ validations/endpoint_validation.py
@@ -1,8 +1,10 @@
 from models.endpoint_model import EndpointCreate
 from repositories.endpoint_repository import EndpointRepository
+from repositories.project_repository import ProjectRepository
 from fastapi import HTTPException, status
 
-async def endpoint_validation(endpoint: EndpointCreate, repo: EndpointRepository):
+
+async def endpoint_validation(endpoint: EndpointCreate, repo: EndpointRepository, repoProject: ProjectRepository):
   
   found = await repo.find_one_by_advance_method({'project_id': endpoint.project_id, 
                                                  'path_url': endpoint.path_url,
@@ -15,3 +17,9 @@ async def endpoint_validation(endpoint: EndpointCreate, repo: EndpointRepository
   if endpoint.status_code == 204 and endpoint.response != None:
     raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE,
                         detail='Satus code 204 not accept response body, it must be None')
+
+  foundProject = await repoProject.find_one_by_id(endpoint.project_id)
+
+  if not foundProject:
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail='Incorrect project')
