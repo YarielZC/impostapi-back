486223d0ff16f8e806a574f82657ce11a2b0d246
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 23:14:14 -0500

    Include apirouter to project manage

--- api/manage.py
+++ api/manage.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from api.manage_routes.endpoint_manage import manage_endpoint_router
+from api.manage_routes.project_manage import manage_project_router
 manage_router = APIRouter(prefix='/dashboard',
                    tags=['Manage'],
                    )
 
 manage_router.include_router(manage_endpoint_router)
+manage_router.include_router(manage_project_router)
