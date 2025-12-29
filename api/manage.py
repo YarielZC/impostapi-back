41371ad652d14cc60bd97e51c84a06010d0cc282
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 21:41:13 -0500

    Create subroute to manage

--- api/manage.py
+++ api/manage.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-
+from api.manage_routes.endpoint_manage import manage_endpoint_router
 manage_router = APIRouter(prefix='/dashboard',
                    tags=['Manage'],
                    )
+
+manage_router.include_router(manage_endpoint_router)
