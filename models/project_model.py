3d94cdf75f7614b44a1b2c21e1ad79ae920f48dd
YarielZC <yarielzamoracueto@gmail.com>
2025-12-28 23:14:56 -0500

    refactorize to do better project model and more powerful the project repository

--- models/project_model.py
+++ models/project_model.py
@@ -1,16 +1,23 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, model_validator
 from datetime import datetime
 
 class ProjectCreate(BaseModel):
   name: str
   description: str | None = None
-  is_public: bool = False
+  permissed: list[str] = []
+  owner_id: str | None = None
+  update_at: datetime = Field(default_factory=lambda: datetime.now())
+  create_at: datetime = Field(default_factory=lambda: datetime.now())
+
+  @model_validator(mode='after')
+  def set_default_permissed_list(self):
+    if not self.owner_id:
+      return self
+    self.permissed = [self.owner_id]
+    return self
 
 class ProjectResponse(ProjectCreate):
   id: str = Field(alias='_id')
-  owner_id: str
-  update_at: datetime = Field(default_factory=lambda: datetime.now())
-  create_at: datetime = Field(default_factory=lambda: datetime.now())
 
   model_config = ConfigDict(
     populate_by_name=True,
--- repositories/project_repository.py
+++ repositories/project_repository.py
@@ -1,5 +1,7 @@
+from fastapi import Depends
 from pymongo.asynchronous.database import AsyncDatabase
 
+from db.connection import get_database
 from models.project_model import ProjectCreate
 from .base_repository import BaseRepository
 
@@ -13,12 +15,21 @@ class ProjectRepository(BaseRepository):
     return await self.db.find_one({field: value})
   
   async def find_one_by_id(self, id: str):
-    return await self.db.find_one({'_id': self._to_object_id(id)})
-  
-  async def insert_one(self, user: ProjectCreate):
-    result = await self.db.insert_one(dict(user))
+    result = await self.db.find_one({'_id': self._to_object_id(id)})
+
+    try:
+      result = self._map_doc(result)
+    except:
+      return None
+    return result
+
+  async def insert_one(self, project: ProjectCreate):
+    result = await self.db.insert_one(project.model_dump())
     return result.inserted_id
     
   async def delete_by_id(self, id: str):
     result = await self.db.delete_one({'_id': id})
     return result.deleted_count
+  
+def get_project_repository(database: AsyncDatabase = Depends(get_database)):
+  return ProjectRepository(database=database)
